a31fb9c0ab17323c6e66e35929902e9c1087a07f
sabrina-440 <sabrinaxjp@gmail.com>
2025-12-25 21:05:51 -0800

    listy bot

---
+++ listy_main.py
@@ -0,0 +1,19 @@
+'''
+/create make a new list
+
+/add add item to already existing list, can specifify list. 
+if not specified it will look for other lists in thread/channel. 
+if no lists in thread then report error, if multiple ask to specify
+can add multiple items seperated by space or use quotes if items have spaces
+prints out list after modification is done
+
+
+/remove (or /rm for short) -remove item from list. all rules of add apply to remove
+
+/delete specify list name to delete, asks for confirmation before doing so
+
+/view view list specified or thread/channel default existing
+
+/show (or /ls) show all current lists
+'''
+
